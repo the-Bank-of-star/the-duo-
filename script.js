6cae7797783dfdcbf3f75a2dbfef5e22d6ada4e3
jabril205 <jabrilslim@gmail.com>
2023-11-30 18:22:09 +0100

    new html tr5555

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 //////////////// id genrater ///////////////////////
 function generateID() {
-  var count = 0;
+  var count = 1;
   return function () {
     return count++;
   };
 }
-function randInt(upperBound) { 
-  return Math.floor(Math.random() * (n + 1)) 
-  }
+
 id = generateID();
 
 var options = {
@@ -43,16 +41,15 @@ var transfer = function (amount) {
 };
 
 function clickii() {
-  let firstName = $("#fname").val()
-  let lastName = $("#lname").val()
-  let amount = Number($("#amount").val())
-  let date = $("#date").val()
-  let account = Acount(firstName, lastName, amount)
-  accounts.push(account)
-  console.log(accounts)
-  ////////////////////////////////////// display account /////////////////////////////////////
+  let firstName = $("#fname").val();
+  let lastName = $("#lname").val();
+  let amount = $("#amount").val();
+  let date = $("#date").val();
+  let account = Acount(firstName, lastName, amount);
+  accounts.push(account);
+  console.log(accounts);
   var new1 =
-    $(`<div><p>${account.firstname} ${account.lastname}</p><p> Amout :${account.solde}$</p><p>account-id :${account.id}  </p><p> the-date : ${account.date}</p> <label > send money babyy</label>
+    $(`<div class ="account-demo" ><p>${account.firstname} ${account.lastname}</p><p> Amout :${account.solde}$</p><p>account-id :${account.id}  </p><p> the-date : ${account.date}</p> <label > send money babyy</label>
         <input type="text" id="amount" name="lname" placeholder="enter your amount" />
         <p>Enter the Account you want to send to</p>
         <input type="text" id="amount" name="lname" placeholder="the name of account" />
@@ -68,47 +65,45 @@ var accounts = [];
 //   console.log(account1)
 
 //    accounts.push(account1,account2)
-
+////////////////////////////////////// display account /////////////////////////////////////
 
 //////////////////////////////////////////function  add an account  ///////////////////////////////////////////
 
 function each(array, func) {
   for (var i = 0; i < array.length; i++) {
-    func(array[i], i)
+    func(array[i], i);
   }
 }
 
 function each_obj(obj, f) {
   for (let key in obj) {
     if (obj.hasOwnProperty(key)) {
-      f(obj[key], key, obj)
+      f(obj[key], key, obj);
     }
   }
 }
 
 ////////////////////////////////// function virement ///////////////////////////////////
- 
-historique = []
-$("#send-button").click(function () {
-
-  var amount = Number($("#send-amount").val())
-  var receiver_id = Number($("#receiver").val())
-  var sender_id =Number()
+historique = [];
+$("send-button").click(function () {
+  var amount = $("#send-amount").val();
+  var receiver_id = $("#receiver").val();
+  var sender_id = $("#sender_id").val();
   each(accounts, function (element, i) {
     each(accounts, function (element2, i) {
       if (element.id === receiver_id && element2.id === sender_id) {
-        element.receive(amount)
-        element2.transfer(amount)
+        element.receive(amount);
+        element2.transfer(amount);
       }
-    })
-  })
-  historique.push({
+    });
+  });
+  historique.psuh({
     sender: sender_id,
     receiver: receiver_id,
     amount: amount,
-    virement_date: new Date().toLocaleString(),
-  })
-})
+    virement_date: new Date().toLocaleDateString(),
+  });
+});
 
 ///////////////////////////////////////////////////////////////////////////////////////////
 function filter(array, predicate) {
@@ -134,7 +129,7 @@ $().click(function () {
         " receiver id : " +
         element.receiver_id +
         " amount : " +
-        element.amount+" date"+element.virement_date
+        element.amount
     );
   });
 });
@@ -142,8 +137,15 @@ $().click(function () {
 /////////////display specefic id ////////////////////
 $().click(function () {
   var id = $("#").val();
-  return filter(historique, function (element, i) {
-    return element.id === id
-  })
+  var filtred = filter(historique, function (element, i) {
+    return element.id === id;
+  });
 
-})
+  return filter(accounts, function (element, i) {
+    return element.id === id;
+  });
+});
+
+console.log();
+//hello
+/// hello 2
